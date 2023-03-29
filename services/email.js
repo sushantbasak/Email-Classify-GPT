aeae0d8961c8c32c3a351cfb4e6f90a7ce427db1
Sushant <sushantbasak73@gmail.com>
2023-03-29 21:48:38 +0530

    refactor: Rename Gmail Service to Email

---
+++ services/email.js
@@ -0,0 +1,27 @@
+const emailAPIProvider = require("../provider/gmailApi");
+
+const getEmailLabels = async () => {
+  const auth = await emailAPIProvider.authorize();
+
+  const val = await getEmailLabels.listLabels(auth);
+};
+
+const getListofEmails = async () => {
+  const auth = await emailAPIProvider.authorize();
+
+  const val = await emailAPIProvider.getMessageIdList(auth, 10);
+};
+
+const getEmailSubjectById = async () => {
+  const auth = await emailAPIProvider.authorize();
+
+  const val = await emailAPIProvider.getEmailSubject(auth);
+  console.log(val.data.snippet);
+  return val.data.snippet;
+};
+
+module.exports = {
+  getEmailLabels,
+  getEmailSubjectById,
+  getListofEmails,
+};
